dee3f230ca906c15566de262f4170d2e975ae0c2
Moustafa Mahmoud <mostafa.93.mahmoud@gmail.com>
2015-06-16 01:00:08 +0200

    added authentication system

--- Karno/main/urls.py
+++ Karno/main/urls.py
@@ -1,4 +1,32 @@
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from main.views import UserRegisteration, UserChangePassword
+from django.contrib.auth.forms import PasswordResetForm
 
 urlpatterns = patterns(
-    '',)
+    '',
+    url(r'^$', TemplateView.as_view(
+        template_name='main/home.html'), name='home'),
+    url(r'^user/new/$', UserRegisteration.as_view(), name='user-new'),
+    url(
+        r'^user/logout/$', auth_views.logout,
+        {'next_page': '/main/'}, name='user-logout'),
+    url(r'^user/login/$', auth_views.login, name='user-login'),
+    url(r'^user/manage/$', TemplateView.as_view(
+        template_name='registration/manage-account.html'), name='user-manage'),
+    url(r'^user/change-password/$', UserChangePassword.as_view(),
+        name='user-change-password'),
+    url(
+        r'^user/reset/$', auth_views.password_reset,
+        {'template_name': 'registration/password_reset.html'},
+        name='user-reset'),
+    url(
+        r'^resetpassword/passwordsent/$',
+        auth_views.password_reset_done,
+        name='password_reset_done'),
+    url(
+        r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+)
--- Karno/main/views.py
+++ Karno/main/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, FormView
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, update_session_auth_hash
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+
 
 # Create your views here.
+class UserRegisteration(CreateView):
+    """
+    Class to handle user Registeration
+    Author: Moustafa
+    """
+    def get(self, request, *args, **kwargs):
+        """
+        Initializes form for user registeration
+        Author: Moustafa
+        """
+        form = UserCreationForm()
+        return render(
+            request, 'registration/user-registeration.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        """
+        Validated Registeration form and auto-login the new user.
+        """
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=request.POST['username'],
+                password=request.POST['password1'])
+            login(request, user)
+            messages.add_message(
+                self.request, messages.INFO,
+                'welcome ' + request.user.username + '.')
+            return HttpResponseRedirect(reverse('home'))
+        return render(
+            request, 'registration/user-registeration.html', {'form': form})
+
+
+class UserChangePassword(FormView):
+    """
+    Class to allow user to change his/her password.
+    Author: Moustafa
+    """
+    def get(self, request, *args, **kwargs):
+        """
+        Initializes form for user registeration
+        Author: Moustafa
+        """
+        form = PasswordChangeForm(user=request.user)
+        return render(
+            request, 'registration/user-change-password.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        """
+        Validated Registeration form and auto-login the new user.
+        Author: Moustafa
+        """
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.add_message(
+                self.request, messages.INFO,
+                'Your Password Has Been Changed Succesfuly.')
+            return HttpResponseRedirect(reverse('home'))
+        return render(
+            request, 'registration/user-change-password.html', {'form': form})
